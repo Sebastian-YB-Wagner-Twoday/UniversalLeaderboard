901ff019831f95872a8baf65b061a2ccdce3e040
SYBWVelliv <g81809-ext@velliv.dk>
2025-01-20 14:01:41 +0100

    chore: cleanup

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/** Unfortunately needed for proper use of ShadCN-vue */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
